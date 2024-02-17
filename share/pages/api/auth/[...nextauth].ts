5ea45e3e0730bc469d0172ee21166c788a3810e2
David Adeogun <adeogundavidt@gmail.com>
2024-02-17 18:21:47 -0500

    Updated .gitignore and package.json

--- share/pages/api/auth/[...nextauth].ts
+++ share/pages/api/auth/[...nextauth].ts
@@ -3,27 +3,20 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 import type { Adapter } from "next-auth/adapters";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/lib/prisma";
-import Auth0Provider from "next-auth/providers/auth0";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authConfig = {
     secret: process.env.NEXTAUTH_SECRET,
-    providers: [Auth0Provider({
-        clientId: process.env.AUTH0_CLIENT_ID as string,
-        clientSecret: process.env.AUTH0_CLIENT_SECRET as string,
-        issuer: process.env.AUTH0_ISSUER_BASE_URL
+    providers: [GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+       
     }),
     ],
     callbacks: {
-        // working on more user session info 
-        // session: async ({session, user}) => {
-        //     // console.log(session, user)
-        //     return session;
-        // },
+       
         async redirect({ url, baseUrl }) {
-            // allows relative callback URLs
             if (url.startsWith("/")) return `${baseUrl}${url}`
-            // allows callback URLs on the same origin
             else if (new URL(url).origin === baseUrl) return url
             return baseUrl
         }
@@ -31,7 +24,7 @@ export const authConfig = {
     adapter: PrismaAdapter(prisma) as Adapter
 } satisfies NextAuthOptions;
 
-export default NextAuth(authConfig);
+export default NextAuth(authConfig); 
 
 
 
