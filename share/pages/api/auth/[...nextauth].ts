eb419c5e00ec2a963dd409e5fd9645fe4a365e31
David Adeogun <adeogundavidt@gmail.com>
2024-02-17 18:37:41 -0500

    Commented out secret in authConfig

--- share/pages/api/auth/[...nextauth].ts
+++ share/pages/api/auth/[...nextauth].ts
@@ -6,7 +6,7 @@ import prisma from "@/lib/prisma";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authConfig = {
-    secret: process.env.NEXTAUTH_SECRET,
+    //secret: process.env.NEXTAUTH_SECRET,
     providers: [GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID as string,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
