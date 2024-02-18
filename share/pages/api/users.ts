02163dacc074560018747491c4668693e4498d28
David Adeogun <adeogundavidt@gmail.com>
2024-02-18 06:35:06 -0500

    ..

--- share/pages/api/users.ts
+++ share/pages/api/users.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { PrismaClient } from "@prisma/client";
 
 
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const prisma = new PrismaClient();
     try {
